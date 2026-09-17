39945dc7eedf284c6fef7153f41bc5c232b6a85d
unknown <maddewithanagechamuka@gmail.com>
2026-09-18 01:43:10 +0530

    importing utils

--- structure/src/main.js
+++ structure/src/main.js
@@ -1,4 +1,6 @@
 import { toggleSidebar } from './modules/ui.js';
+import * as Utils from './modules/utils.js';
+
 // 1. Find the elements on the page
 const menuBtn = document.getElementById('menu-btn');
 menuBtn.addEventListener('click', toggleSidebar);

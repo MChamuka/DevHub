7b013a3295cb7d0954ddb55f72bee48950b47584
unknown <maddewithanagechamuka@gmail.com>
2026-09-18 02:13:23 +0530

    utils main added

--- structure/src/main.js
+++ structure/src/main.js
@@ -4,3 +4,10 @@ import * as Utils from './modules/utils.js';
 // 1. Find the elements on the page
 const menuBtn = document.getElementById('menu-btn');
 menuBtn.addEventListener('click', toggleSidebar);
+
+// Access the functions inside the Utils object
+const newTaskId = Utils.generateId();
+const projectProgress = Utils.calculatePercentage(8, 10);
+
+console.log(`Created new task with ID: ${newTaskId}`);
+console.log(`Project is ${projectProgress}% complete.`);

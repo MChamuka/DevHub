0bec1b7dd79eb806488bd4a26bea0a5c3a1728f3
unknown <maddewithanagechamuka@gmail.com>
2026-09-19 08:39:58 +0530

    storage data added

---
+++ structure/src/modules/storage.js
@@ -0,0 +1,14 @@
+// src/modules/storage.js
+
+const Storage = {
+    save: function(key, data) {
+        localStorage.setItem(key, JSON.stringify(data));
+        console.log(`Saved data to ${key}`);
+    },
+    load: function(key) {
+        return JSON.parse(localStorage.getItem(key));
+    }
+};
+
+// This is the ONE primary export for this file
+export default Storage;

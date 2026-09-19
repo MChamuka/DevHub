25716782376682bcdd71ee00bbbf4ec563345873
unknown <maddewithanagechamuka@gmail.com>
2026-09-19 17:27:05 +0530

    users data

--- structure/src/main.js
+++ structure/src/main.js
@@ -1,5 +1,6 @@
 import { toggleSidebar } from './modules/ui.js';
 import * as Utils from './modules/utils.js';
+import AppStorage from './modules/storage.js';
 
 // 1. Find the elements on the page
 const menuBtn = document.getElementById('menu-btn');
@@ -11,3 +12,6 @@ const projectProgress = Utils.calculatePercentage(8, 10);
 
 console.log(`Created new task with ID: ${newTaskId}`);
 console.log(`Project is ${projectProgress}% complete.`);
+
+const dummyProject = { name: "Redesign Dashboard", status: "Active" };
+AppStorage.save('currentProject', dummyProject);
---
+++ structure/src/modules/users.js
@@ -0,0 +1,10 @@
+// src/modules/users.js
+import { generateId } from './utils.js';
+
+export default class User {
+    constructor(name, email) {
+        this.id = generateId(); // Automatically assigns an ID
+        this.name = name;
+        this.email = email;
+    }
+}

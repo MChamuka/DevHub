235a2ed183721fb33da26e8fa914dac858bc452f
unknown <maddewithanagechamuka@gmail.com>
2026-09-20 03:21:40 +0530

    mmodified mmain.js o all imports getting from index.js

--- structure/src/main.js
+++ structure/src/main.js
@@ -1,9 +1,7 @@
 import { toggleSidebar } from './modules/ui.js';
 import * as Utils from './modules/utils.js';
 import AppStorage from './modules/storage.js';
-import User from './modules/users.js';
-import Task from './modules/tasks.js';
-import Project from './modules/projects.js';
+import {User,Task,Project} from './modules/index.js';
 
 // 1. Find the elements on the page
 const menuBtn = document.getElementById('menu-btn');

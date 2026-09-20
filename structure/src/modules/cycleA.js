3475faf7b6b357b724b8f9c7cdbde6fc0d3a5e32
unknown <maddewithanagechamuka@gmail.com>
2026-09-20 21:11:17 +0530

    cycle A and B added

---
+++ structure/src/modules/cycleA.js
@@ -0,0 +1,3 @@
+import { secretB } from './cycleB.js';
+export const secretA = "Message from A";
+console.log("File A loaded! B's secret is:", secretB);
---
+++ structure/src/modules/cycleB.js
@@ -0,0 +1,3 @@
+import { secretA } from './cycleA.js';
+export const secretB = "Message from B";
+console.log("File B loaded! A's secret is:", secretA);
